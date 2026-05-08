fc21a4f45db5c0558fd65581024545c1c66c2e8d
flexxy-07 <sumitrpt007@gmail.com>
2026-05-09 00:46:54 +0530

    setup firebase and added env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
-import 'package:vision_companion/core/theme/app_theme.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+import 'core/theme/app_theme.dart';
+import 'firebase_env_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(options: FirebaseEnvOptions.currentPlatform);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
   runApp(const VisionCompanionApp());
 }
 
